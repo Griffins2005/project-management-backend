f0a89628e93c8c75ca96ae3c43740a6d1790ea6f
Griffins Kiptanui Lelgut <134501193+Griffins2005@users.noreply.github.com>
2025-02-05 00:57:43 -0500

    Update google sign url

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const passport = require("passport");
+
 const router = express.Router();
 
-router.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+router.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"],  
+  prompt: "select_account" 
+}));
 
-router.get(
-  "/google/callback",
-  passport.authenticate("google", { failureRedirect: "https://project-management-tool-app.onrender.com" }),
-  (req, res) => {
-    res.redirect("https://project-management-tool-app.onrender.com/project");
-  }
-);
+router.get("/auth/google/callback", passport.authenticate("google", {
+  failureRedirect: "/login",
+  successRedirect: "https://project-management-tool-app.onrender.com/project", 
+}));
 
 router.get("/current_user", (req, res) => {
   res.send(req.user);
