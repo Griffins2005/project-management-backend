6fc78aa746ea957003cbe6cf87b6bc92077052f6
Griffins Kiptanui Lelgut <134501193+Griffins2005@users.noreply.github.com>
2025-02-05 11:30:09 -0500

    Update app.get

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
