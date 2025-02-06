da1a8c4857f5bc1b72f7c3bc03f8fd60b4342eca
Griffins2005 <Griffins2005>
2025-02-05 20:57:58 -0500

    clean up , deleted the console log

--- server.js
+++ server.js
@@ -4,26 +4,15 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 const passport = require("./passport.js");
-const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-const taskRoutes = require("./routes/task");
-const authRoutes = require("./routes/auth");
-const priorityRoutes = require("./routes/priority");
-const teamRoutes = require("./routes/team");
-const statusRoutes = require("./routes/progress");
-const projectRoutes = require("./routes/project");
-
 app.use(
   cors({
-    origin: ["https://project-management-tool-app.onrender.com"],
+    origin: "https://project-management-tool-app.onrender.com",
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
@@ -39,12 +28,30 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+const taskRoutes = require("./routes/task");
+const authRoutes = require("./routes/auth");
+const priorityRoutes = require("./routes/priority");
+const teamRoutes = require("./routes/team");
+const statusRoutes = require("./routes/progress");
+const projectRoutes = require("./routes/project");
+
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/team", teamRoutes);
 app.use("/api/statuses", statusRoutes);
 app.use("/api/priorities", priorityRoutes);
 app.use("/api/projects", projectRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("*", (req, res) => {
+  if (req.path.startsWith("/api")) {
+    return res.status(404).json({ error: "API route not found" });
+  }
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 mongoose
   .connect(process.env.MONGO_URI, {})
@@ -58,6 +65,3 @@ mongoose
   .catch((err) => {
     console.error("MongoDB connection failed:", err.message);
   });
-
-console.log("Google Client ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("Google Client Secret:", process.env.GOOGLE_CLIENT_SECRET);
